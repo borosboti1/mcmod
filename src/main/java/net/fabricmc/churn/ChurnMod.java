d8b9ecc57dd217a32be19cc1daa630d5869c1c4a
borosboti1 <148892133+borosboti1@users.noreply.github.com>
2025-11-30 03:08:52 +0100

    elsosikeres

--- src/main/java/net/fabricmc/churn/ChurnMod.java
+++ src/main/java/net/fabricmc/churn/ChurnMod.java
@@ -1,14 +1,11 @@
 package net.fabricmc.churn;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class ChurnMod implements ModInitializer {
     @Override
     public void onInitialize() {
-        // Új Fabric API parancs regisztráció
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
-            ChurnCommand.register(dispatcher);
-        });
+        // Jelenleg nincs parancs regisztráció, mert a ChurnCommand nincs kifejlesztve
+        System.out.println("Churn mod initialized!");
     }
 }
