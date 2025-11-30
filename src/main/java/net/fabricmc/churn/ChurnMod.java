1d4f808194fb11aa0464bb5f3fa08378423d9837
borosboti1 <148892133+borosboti1@users.noreply.github.com>
2025-11-30 11:33:08 +0100

    verz 1.1

--- src/main/java/net/fabricmc/churn/ChurnMod.java
+++ src/main/java/net/fabricmc/churn/ChurnMod.java
@@ -1,11 +1,33 @@
 package net.fabricmc.churn;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ChurnMod implements ModInitializer {
+    public static final String MOD_ID = "churn";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+    
     @Override
     public void onInitialize() {
-        // Jelenleg nincs parancs regisztráció, mert a ChurnCommand nincs kifejlesztve
-        System.out.println("Churn mod initialized!");
+        LOGGER.info("=== CHURN MOD INICIALIZÁLÁS ===");
+        LOGGER.info("1. Mod main class betöltve");
+        
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+            LOGGER.info("2. CommandRegistrationCallback meghívva");
+            LOGGER.info("3. Environment: " + environment.toString());
+            LOGGER.info("4. Dispatcher: " + dispatcher.toString());
+            
+            try {
+                net.fabricmc.churn.command.ChurnCommand.register(dispatcher);
+                LOGGER.info("5. ChurnCommand.register() sikeres");
+            } catch (Exception e) {
+                LOGGER.error("6. Hiba a parancs regisztrációban: " + e.getMessage());
+                e.printStackTrace();
+            }
+        });
+        
+        LOGGER.info("=== CHURN MOD INICIALIZÁLÁS BEFEJEZVE ===");
     }
 }
--- src/main/java/net/fabricmc/churn/command/ChurnCommand.java
+++ src/main/java/net/fabricmc/churn/command/ChurnCommand.java
@@ -2,101 +2,25 @@ package net.fabricmc.churn.command;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.arguments.StringArgumentType;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 
 import static net.minecraft.server.command.CommandManager.*;
 
 public class ChurnCommand {
     
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
-        dispatcher.register(literal("churn")
-            .then(literal("start")
-                .then(argument("world", StringArgumentType.word())
-                    .then(argument("radius", IntegerArgumentType.integer(16))
-                        .then(argument("options", StringArgumentType.greedyString()).executes(ctx -> executeStart(ctx)))
-                    )
-                )
-            )
-            .then(literal("status")
-                .then(argument("options", StringArgumentType.greedyString()).executes(ctx -> executeStatus(ctx)))
-            )
-            .then(literal("cancel").executes(ctx -> executeCancel(ctx)))
-            .then(literal("pause").executes(ctx -> executePause(ctx)))
-            .then(literal("resume")
-                .then(argument("path", StringArgumentType.string()).executes(ctx -> executeResume(ctx)))
-            )
-            .then(literal("clean-checkpoints")
-                .then(argument("path", StringArgumentType.string()).executes(ctx -> executeCleanCheckpoints(ctx)))
-            )
-            .then(literal("postprocess")
-                .then(argument("path", StringArgumentType.string()).executes(ctx -> executePostprocess(ctx)))
-            )
+        System.out.println("ChurnCommand.register() ELKEDZŐDIK");
+        
+        dispatcher.register(
+            literal("churntest")
+                .executes(context -> {
+                    context.getSource().sendMessage(Text.literal("§aChurn teszt parancs MŰKÖDIK!"));
+                    System.out.println("Churn teszt parancs végrehajtva");
+                    return 1;
+                })
         );
-    }
-
-    private static int executeStart(CommandContext<ServerCommandSource> ctx) {
-        // Implementáció
-        return 1;
-    }
-
-    private static int executeStatus(CommandContext<ServerCommandSource> ctx) {
-        // Implementáció
-        return 1;
-    }
-
-    private static int executeCancel(CommandContext<ServerCommandSource> ctx) {
-        // Implementáció
-        return 1;
-    }
-
-    private static int executePostprocess(CommandContext<ServerCommandSource> ctx) {
-        // Implementáció
-        return 1;
-    }
-
-    private static int executeCleanCheckpoints(CommandContext<ServerCommandSource> ctx) {
-        // Implementáció
-        return 1;
-    }
-
-    private static int executePause(CommandContext<ServerCommandSource> ctx) {
-        // Implementáció
-        return 1;
-    }
-
-    private static int executeResume(CommandContext<ServerCommandSource> ctx) {
-        // Implementáció
-        return 1;
-    }
-
-    private static String safeGetString(CommandContext<ServerCommandSource> ctx, String name) {
-        return StringArgumentType.getString(ctx, name);
-    }
-
-    private static String safeGetStringOptional(CommandContext<ServerCommandSource> ctx, String name) {
-        try {
-            return StringArgumentType.getString(ctx, name);
-        } catch (Exception e) {
-            return null;
-        }
-    }
-
-    private static int safeGetInt(CommandContext<ServerCommandSource> ctx, String name, int def) {
-        try {
-            return IntegerArgumentType.getInteger(ctx, name);
-        } catch (Exception e) {
-            return def;
-        }
-    }
-
-    private static Integer safeGetOptionalInt(CommandContext<ServerCommandSource> ctx, String name) {
-        try {
-            return IntegerArgumentType.getInteger(ctx, name);
-        } catch (Exception e) {
-            return null;
-        }
+        
+        System.out.println("ChurnCommand.register() BEFEJEZVE");
     }
 }
